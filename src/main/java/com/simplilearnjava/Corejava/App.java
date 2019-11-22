07e3179623c6eeee8a6c88216ad00eb531951d07
james jacob <jamesjacob84@gmail.com>
2019-11-22 04:46:35 +0000

    maven project

---
+++ src/main/java/com/simplilearnjava/Corejava/App.java
@@ -0,0 +1,13 @@
+package com.simplilearnjava.Corejava;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
